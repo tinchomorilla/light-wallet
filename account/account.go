e54ee0f68e8004514e07798320964ff917d4a278
Tincho Morilla <mmorilla@fi.uba.ar>
2025-03-05 00:42:50 -0300

    GenerateNewAccount just creates an account; it doesn't encrypt the private key. It's not its responsibility

--- account/account.go
+++ account/account.go
@@ -9,7 +9,6 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
-	"github.com/tinchomorilla/light-wallet/encryption"
 )
 
 // GenerateNewAccount generates a new Ethereum account and returns the address, along with the encrypted private key.
@@ -19,33 +18,23 @@ func GenerateNewAccount(password string) (common.Address, *ecdsa.PrivateKey, err
 		return common.Address{}, nil, fmt.Errorf("password cannot be empty")
 	}
 
-    // Create a new keypair
-    privateKey, err := crypto.GenerateKey()
-    if err != nil {
-        return common.Address{}, nil, fmt.Errorf("failed to generate private key: %v", err)
-    }
+	// Create a new keypair
+	privateKey, err := crypto.GenerateKey()
+	if err != nil {
+		return common.Address{}, nil, fmt.Errorf("failed to generate private key: %v", err)
+	}
 
-    // Encrypt the private key with the password
-    encryptedPrivateKey, err := encryption.EncryptPrivateKey(privateKey, password)
-    if err != nil {
-        return common.Address{}, nil, fmt.Errorf("failed to encrypt private key: %v", err)
-    }
+	// Derive the account address from the public key
+	address := crypto.PubkeyToAddress(privateKey.PublicKey)
 
-	// Should store the encrypted private key in a secure location
-	fmt.Println("Encrypted private key:", encryptedPrivateKey)
-
-    // Derive the account address from the public key
-    address := crypto.PubkeyToAddress(privateKey.PublicKey)
-
-    return address, privateKey, nil
+	return address, privateKey, nil
 }
-
 
 // GetBalance fetches the balance of an Ethereum address.
 func GetBalance(client *ethclient.Client, address common.Address) (*big.Int, error) {
-    balance, err := client.BalanceAt(context.Background(), address, nil)
-    if err != nil {
-        return nil, fmt.Errorf("failed to get balance: %v", err)
-    }
-    return balance, nil
+	balance, err := client.BalanceAt(context.Background(), address, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get balance: %v", err)
+	}
+	return balance, nil
 }
